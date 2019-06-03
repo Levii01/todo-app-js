77bbb8b7998c220ed49fdb004b3353e103d19f1f
Levii01 <m.sedziak01@gmail.com>
2019-06-03 08:24:48 +0200

    Add view and change root page

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ var app = express();
 
 //root(/) URL
 app.get('/', function (req, res) {
-//when we visit the root URL express will respond with 'Hello World'
-  res.send('Hello World!');
+  // res.send('Hello World!');
+  res.render('index');
 });
+
 
 //set port to 3000
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
+
+app.set('view engine', 'ejs');
