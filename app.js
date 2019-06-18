bb9623969c114744a00bf559cd25f8deefa7e505
Levii01 <m.sedziak01@gmail.com>
2019-06-18 14:59:06 +0200

    Delete teask action

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var tasks = ["buy socks", "practise with nodejs"];
+var completedTasks = ["finish jquery"];
+app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 //root(/) URL
 app.get('/', function (req, res) {
   // res.send('Hello World!');
-  res.render('index', { tasks: tasks});
+  res.render('index', { tasks: tasks, completedTasks: completedTasks });
 });
 
+function deleteTaskFromTasks(task) {
+  tasks.splice(tasks.indexOf(task), 1);
+}
 
 //set port to 3000
 app.listen(3000, function () {
@@ -23,5 +28,19 @@ app.post('/addtask', function (req, res) {
   var newTask = req.body.newtask;
   tasks.push(newTask);
   res.redirect("/");
-  // res.render('index')
+});
+
+app.post("/removetask", function(req, res) {
+  var checkedTasks = req.body.check;
+  //check for the "typeof" the different completed task, then add into the complete task
+  if (typeof checkedTasks === "string") {
+    completedTasks.push(checkedTasks);
+    deleteTaskFromTasks(checkedTasks)
+  } else if (typeof checkedTasks === "object") {
+    checkedTasks.map( task => {
+    completedTasks.push(task);
+    deleteTaskFromTasks(checkedTasks)
+    })
+  }
+  res.redirect("/");
 });
